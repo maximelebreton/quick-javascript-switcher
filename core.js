c6b95459088ae70b6c8064fb224a8be806a160ab
maximelebreton <mail@maximelebreton.com>
2012-10-26 21:35:59 +0200

    - many fixes - v1.2.0

--- core.js
+++ core.js
@@ -13,6 +13,7 @@ var chromeContentSettings = chrome.contentSettings;
 /* currently (chrome 16), infobars is not implemented (only experimental...) */
 var chromeInfobars = chrome.infobars;
 
+
 init();
 
 if(chromeContentSettings) {
@@ -24,71 +25,7 @@ if(chromeContentSettings) {
 		setting,
 		tabId,
 		matchForbiddenOrigin;
-	
-	function updateIcon(setting) {
-			chrome.browserAction.setIcon({path:"icon-" + setting + ".png"});
-	}
-	
-	function getSettings() {
-		chrome.tabs.getSelected(undefined, function(tab) {
-			incognito = tab.incognito;
-			url = tab.url;
-			tabId = tab.id;
-			
-			//console.info("Current tab settings : "+url);
-			chromeContentSettings.javascript.get({
-				'primaryUrl': url,
-				'incognito': incognito
-			},
-			function(details) {
-				//console.info("Current tab settings : "+url);
-				matchForbiddenOrigin = url.match(forbiddenOrigin,'');
-				matchForbiddenOrigin ? updateIcon("inactive") : updateIcon(details.setting);
-				
-			});
-		});
-	}
-	
-	function changeSettings() {
-		if (!matchForbiddenOrigin) {
-			chromeContentSettings.javascript.get({
-				'primaryUrl': url,
-				'incognito': incognito
-			},
-			function(details) {
-				setting = details.setting;
-				if (setting) {
-					var pattern = /^file:/.test(url) ? url : url.match(extractHostname)[0]+'/*';
-					// old method : url.replace(/\/[^\/]*?$/, '/*')
-					var newSetting = (setting == 'allow' ? 'block' : 'allow'); 
-					chromeContentSettings.javascript.set({
-						'primaryPattern': pattern,
-						'setting': newSetting,
-						'scope': (incognito ? 'incognito_session_only' : 'regular')
-					});
-					updateIcon(newSetting);
-					if (prefs.autoRefresh) {
-						chrome.tabs.reload(tabId);
-					}
-					//console.info("javascript is now "+newSetting+"ed on "+pattern);
-				}
-				else {
-					//console.error("error, the setting is "+setting);
-				}
-			});
-		}
-		else {
-			
-			if(chromeInfobars) {
-				chromeInfobars.show({"tabId": tabId, "path": "infobar.html"});
-			}
-			else {
-				//console.error("You can't disable javascript on "+url);
-			}
-			
-		}
-	}
-	
+
 	chrome.tabs.onUpdated.addListener(function(tabId, props, tab) {
 		// Prevent multiple calls
 		if (props.status == "loading" && tab.selected) {
@@ -96,42 +33,212 @@ if(chromeContentSettings) {
 			getSettings();
 		}
 	});
-	
-	chrome.tabs.onSelectionChanged.addListener(function(tabId, props) {
-		//console.info("onSelectionChanged");
+
+	chrome.tabs.onHighlighted.addListener(function() {
+		//console.info("onHighlighted");
 		getSettings();
 	});
-	
-	chrome.tabs.getSelected(null, function(tab) {
-		//console.info("getSelected");
+
+	chrome.windows.onFocusChanged.addListener(function() {
+		//console.info("onFocusChanged");
 		getSettings();
 	});
-	
+
+	chrome.windows.getCurrent(function(win) {
+		chrome.tabs.query( {'windowId': win.id, 'active': true}, function(){
+			//console.info("getCurrent");
+			getSettings();
+		});
+	});
+
 	chrome.browserAction.onClicked.addListener(changeSettings);
-		
+
+} else {
+	chrome.browserAction.onClicked.addListener(openJsPanel.call());
 }
-else {
+
+function updateIcon(setting) {
+		chrome.browserAction.setIcon({path:"icon-" + setting + ".png"});
+}
+
+function getSettings() {
+	chrome.tabs.getSelected(undefined, function(tab) {
+		incognito = tab.incognito;
+		url = tab.url;
+		tabId = tab.id;
+		
+		//console.info("Current tab settings : "+url);
+		chromeContentSettings.javascript.get({
+			'primaryUrl': url,
+			'incognito': incognito
+		},
+		function(details) {
+			//console.info("Current tab settings : "+url);
+			url ? matchForbiddenOrigin = url.match(forbiddenOrigin,'') : matchForbiddenOrigin = true;
+			matchForbiddenOrigin ? updateIcon("inactive") : updateIcon(details.setting);				
+		});
+	});
+}
+
+function changeSettings() {
+	if (!matchForbiddenOrigin) {
+		chromeContentSettings.javascript.get({
+			'primaryUrl': url,
+			'incognito': incognito
+		},
+		function(details) {
+
+			setting = details.setting;
+			if (setting) {
+				var pattern = /^file:/.test(url) ? url : url.match(extractHostname)[0]+'/*';
+				
+				// old method : url.replace(/\/[^\/]*?$/, '/*')
+				var newSetting = (setting == 'allow' ? 'block' : 'allow');
+				chromeContentSettings.javascript.set({
+					'primaryPattern': pattern,
+					'setting': newSetting,
+					'scope': (incognito ? 'incognito_session_only' : 'regular')
+				});
+				
+				updateIcon(newSetting);
+
+				if (prefs.autoRefresh) {
+					chrome.tabs.reload(tabId);
+				}
+
+				setLocalStorageRule(pattern, newSetting);
+
+				//console.info("javascript is now "+newSetting+"ed on "+pattern);
+			}
+			else {
+				//console.error("error, the setting is "+setting);
+			}
+		});
+	}
+	else {
+		
+		if(chromeInfobars) {
+			chromeInfobars.show({"tabId": tabId, "path": "infobar.html"});
+		}
+		else {
+			//console.error("You can't disable javascript on "+url);
+		}
+		
+	}
+}
+
+
+function getLocalStorageRules() {
+	return window.localStorage.qjs_rules;
+}
+
+function setLocalStorageRule(pattern, newSetting) {
+
+	if (!incognito) {
+
+		var keyExist = false;
+
+		if (rules.length) {
+			for(i = 0; i < rules.length; i++) {
+				if(pattern == rules[i].primaryPattern) {
+					rules[i].setting = newSetting;
+					keyExist = true;
+					break;
+				}
+			}
+		}
+
+		if (!keyExist) {
+			rules.push({
+				'primaryPattern': pattern,
+				'setting': newSetting,
+				'scope': (incognito ? 'incognito_session_only' : 'regular')
+			});
+		}
+
+		window.localStorage.setItem('qjs_rules',JSON.stringify(rules));
+
+	}
+
+}
+
+function importRules(localStorageRules) {
+
+	var rules = localStorageRules;
+
+	if (rules.length) {
+		for(i = 0; i < rules.length; i++) {
+
+			chrome.contentSettings.javascript.set({
+				'primaryPattern': rules[i].primaryPattern,
+				'setting': rules[i].setting,
+				'scope': rules[i].scope
+			});
+		}
+	}
+
+	window.localStorage.setItem('qjs_rules',JSON.stringify(rules));
+
+}
+
+function clearRules(arg) {
 	
-	chrome.browserAction.onClicked.addListener(openJsPanel);
-	
+	if (arg == "contentSettings" || arg === undefined) {
+		chromeContentSettings.javascript.clear({'scope': (incognito ? 'incognito_session_only' : 'regular')});
+	}
+	if (arg == "localStorage" || arg === undefined) {
+		rules = [];
+		window.localStorage.setItem('qjs_rules',[]);
+	}
 }
 
 function getLocalStoragePrefs() {
 	
-	if (!localStorage.prefs_QJS) {
-		localStorage.prefs_QJS = JSON.stringify({ "showContextMenu": true, "autoRefresh": true });
+	// qjs_prefs
+	if (!window.localStorage.qjs_prefs) {
+		window.localStorage.qjs_prefs = JSON.stringify({ "showContextMenu": true, "autoRefresh": true });
 	}
-	
-	prefs = JSON.parse(localStorage.prefs_QJS);
+	prefs = JSON.parse(window.localStorage.qjs_prefs);
+
+	// qjs_rules
+	if (!window.localStorage.qjs_rules) {
+		clearRules("localStorage");
+	} else {
+		rules = JSON.parse(window.localStorage.qjs_rules);
+	}
+
+	// qjs_version
+	var currentVersion = getVersion();
+	var previousVersion = window.localStorage.qjs_version;
+	if (currentVersion != previousVersion) {
+		if (typeof previousVersion == 'undefined') {
+			onInstall();
+		} else {
+			onUpdate();
+		}
+		window.localStorage.qjs_version = currentVersion;
+	}
 
 }
 
+// Check if the version has changed.
+function onInstall() {
+	if (rules.length) {	importRules(rules);	}
+}
+function onUpdate() {
+	if (rules.length) {	importRules(rules);	}
+}
+function getVersion() {
+	var details = chrome.app.getDetails();
+	return details.version;
+}
+
 function toggleContextMenu() {
-	
+
 	if (prefs.showContextMenu && !contextMenuId) {
 		
 		contextMenuId = chrome.contextMenus.create({
-			"title" : "Go to Chrome JavaScript settings",
+			"title" : "Go to JavaScript settings",
 			"type" : "normal",
 			"contexts" : ["all"],
 			"onclick" : openJsPanel()
@@ -145,15 +252,13 @@ function toggleContextMenu() {
 		contextMenuId = null;
 		
 	}
-	
+
 }
 
 function openJsPanel() {
-	
 	return function(info, tab) {
-		chrome.tabs.create({"url":"chrome://settings/content#javascript", "selected":true});
+		chrome.tabs.create({"url":"chrome://chrome/settings/contentExceptions#javascript", "selected":true});
 	};
-	
 }
 
 function init() {
--- options.js
+++ options.js
@@ -1,21 +1,49 @@
-// Make sure the checkbox checked state gets properly initialized from the
-// saved preference.
+
+
+document.addEventListener("webkitvisibilitychange", showRules, false);
+
+function save() {
+	var prefs = JSON.parse(window.localStorage.qjs_prefs);
+	prefs.showContextMenu = document.getElementById("contextMenu").checked;
+	prefs.autoRefresh = document.getElementById("autoRefresh").checked;
+	window.localStorage.qjs_prefs = JSON.stringify(prefs);
+	
+	chrome.extension.getBackgroundPage().init();
+}
+function showRules() {
+	document.getElementById("qjs_rules").value = chrome.extension.getBackgroundPage().getLocalStorageRules();
+}
+
 window.onload = function() {
-	var prefs = JSON.parse(localStorage.prefs_QJS);
+
+	var prefs = JSON.parse(window.localStorage.qjs_prefs);
+
+	showRules();
+
 	document.getElementById("contextMenu").checked = prefs.showContextMenu;
 	document.getElementById("autoRefresh").checked = prefs.autoRefresh;
-		
-	function save() {
-		var prefs = JSON.parse(localStorage.prefs_QJS);
-		prefs.showContextMenu = document.getElementById("contextMenu").checked;
-		prefs.autoRefresh = document.getElementById("autoRefresh").checked;
-		localStorage.prefs_QJS = JSON.stringify(prefs);	
-		
-		chrome.extension.getBackgroundPage().init();
-	}
 	
 	document.getElementById("contextMenu").onclick = function() { save(); };
 	document.getElementById("autoRefresh").onclick = function() { save(); };
+	
+	document.getElementById("openJavascriptSettings").onclick = chrome.extension.getBackgroundPage().openJsPanel();
+
+	document.getElementById("clearJavascriptSettings").onclick = function() {
+		chrome.extension.getBackgroundPage().clearRules("contentSettings");
+		chrome.extension.getBackgroundPage().openJsPanel().call();
+	};
+
+	document.getElementById("importRules").onclick = function() {
+		if (document.getElementById("qjs_rules").value !== "") {
+			chrome.extension.getBackgroundPage().importRules(JSON.parse(document.getElementById("qjs_rules").value));
+			chrome.extension.getBackgroundPage().openJsPanel().call();
+		}
+	};
+
+	document.getElementById("clearLocalStorageRules").onclick = function() {
+		chrome.extension.getBackgroundPage().clearRules("localStorage");
+		showRules();
+	};
 
 }
 
